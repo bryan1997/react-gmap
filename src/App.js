85ddf12ead6282bab3af37106c0fe115b6c9cd5d
Hoo Jun Zhe <hoojunzhe@hotmail.com>
2022-03-22 09:33:16 +0800

    ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,143 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, {useRef, useCallback} from "react";
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
+
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+  ComboboxOptionText,
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
+
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import Input from '@material-ui/core/Input';
+
+ 
+const libraries = ["places"];
+
+const mapContainerStyle = {
+  width: '100vw',
+  height: '100vh',
+};
+
+const center = {
+  lat: 15.8700 ,
+  lng: 100.9925,
+};
+
+
+export default function App() {
+    const {isLoaded, loadError} = useLoadScript({
+      googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+      libraries,
+    });
+
+    const mapRef = React.useRef();
+    const onMapLoad = React.useCallback((map) => {
+      mapRef.current = map;
+    }, []);
+
+    const panTo = React.useCallback(({lat, lng}) => {
+      mapRef.current.panTo({lat, lng});
+      mapRef.current.setZoom(14);
+    }, []);
+
+if (loadError) return "Error loading maps";
+if (!isLoaded) return "Loading Maps";
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>Assignment</h1>
+
+    <Search panTo = {panTo} /> 
+
+     <GoogleMap 
+     mapContainerStyle={mapContainerStyle}
+     zoom={8}
+     center={center}
+     onLoad={onMapLoad}
+     ></GoogleMap>
     </div>
   );
 }
 
-export default App;
+function Search( { panTo }) {
+  const { ready, value, suggestions: {status, data}, setValue, clearSuggestion } = usePlacesAutocomplete({
+    requestOptions: {
+      location: { lat: () => 3.1390 , lng: () => 101.6869 },
+      radius: 200 * 1000,
+    }
+  });
+
+  console.log(data);
+
+
+  
+  function geoCode (parameter) {
+    console.log(parameter)
+    getGeocode(parameter)
+  .then((results) => getLatLng(results[0]))
+  .then((latLng) => {
+    const { lat, lng } = latLng;
+
+    console.log("Coordinates: ", { lat, lng });
+  })
+  .catch((error) => {
+    console.log("Error: ", error);
+
+    
+  });
+  
+  }
+
+  
+
+return (
+<div className = "search">
+      <Autocomplete
+        freeSolo
+        id="free-solo-2-demo"
+        disableClearable
+        options={data}
+        getOptionLabel={(option) => option.description}
+        getOptionSelected={(option, value) => option.description === value.description}
+        onChange={(e) => {
+          geoCode(e.target.value);
+        }}
+        renderInput={(params) => (
+          <TextField
+          {...params}
+            label="Search input"
+            InputProps={{
+              ...params.InputProps,
+              type: 'search',
+            }}
+
+            onChange={(e) => {
+              setValue(e.target.value);
+            
+            
+            
+            
+            }}
+          />
+        )}
+      />
+</div>
+)};
+
